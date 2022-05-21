6db90a193894334fc377f96d690f5c136e7f1770
Adrian Escalante <adresc@gmail.com>
2022-05-21 14:09:04 -0500

    quitar boton

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,11 +16,11 @@ const NavBar = () => {
         <Link to='/robots' className="mr-5 hover:text-gray-900">Robots</Link>
         <Link to='/salvarfavs' className="mr-5 hover:text-gray-900">Salvar Lista</Link>
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+        {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
-        </button>
+        </button> */}
     </div>
     </header>
   )
